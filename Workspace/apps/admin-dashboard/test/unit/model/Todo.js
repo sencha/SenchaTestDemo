24bedc044793e876f6e5268bbc83bba5d3a5b80f
nickrnet <nick.rogers@sencha.com>
2016-04-12 20:35:20 -0500

    Properly commenting unit tests.

--- Workspace/apps/admin-dashboard/test/unit/model/Todo.js
+++ Workspace/apps/admin-dashboard/test/unit/model/Todo.js
@@ -1,72 +1,129 @@
 describe('Todo Tests', function() {
 
+    var taskName = 'Do this later';
+
     describe('with a not completed todo item', function() {
         beforeEach( function() {
             this.startTime = new Date().getTime();
-            this.data = {
+            // Create a not completed item
+            this.todo = new Admin.model.Todo({
                 id: 99,
                 task: 'Do this now',
                 done: false
-            };
-            this.todo = new Admin.model.Todo(this.data);
+            });
         });
 
-        describe('by field name', function() {
-            it('should update task name', function () {
-                var taskName = 'Do this later';
+        describe('set fields by name', function() {
+            it('should not set done or completedDate when changing another field', function () {
+                // Verify the constructor does not complete the item
+                expect(this.todo.data.done).toBe(false);
+                expect(this.todo.data.completedDate).toBe(null);
+
+                // Set a field
                 this.todo.set('task', taskName);
+
                 expect(this.todo.data.task).toBe(taskName);
+                expect(this.todo.data.done).toBe(false);
                 expect(this.todo.data.completedDate).toBe(null);
             });
 
-            it('should complete the todo', function () {
+            it('should set completedDate when done is set', function () {
+                // Verify the constructor does not complete the item
+                expect(this.todo.data.done).toBe(false);
+                expect(this.todo.data.completedDate).toBe(null);
+
+                // Complete the item
                 this.todo.set('done', true);
+
                 expect(this.todo.data.completedDate).not.toBe(null);
 
+                // Calculate times
                 var todoEndTime = this.todo.data.completedDate.getTime() - this.startTime,
-                    testEndTime = new Date().getTime() - this.startTime;
+                    endTime = new Date().getTime() - this.startTime;
 
-                expect(todoEndTime).toBeGreaterThan(0);
+                expect(todoEndTime).not.toBeLessThan(0);
+                expect(todoEndTime).not.toBeGreaterThan(endTime);
+            });
 
-                if (todoEndTime < testEndTime) {
-                    expect(todoEndTime).toBeLessThan(testEndTime);
-                } else {
-                    expect(todoEndTime).toBe(testEndTime);
-                }
+            it('should unset completedDate when done is unset', function () {
+                // Verify the constructor does not complete the item
+                expect(this.todo.data.done).toBe(false);
+                expect(this.todo.data.completedDate).toBe(null);
+
+                // Complete the item
+                this.todo.set('done', true);
+
+                expect(this.todo.data.completedDate).not.toBe(null);
+
+                // Calculate times
+                var todoEndTime = this.todo.data.completedDate.getTime() - this.startTime,
+                    endTime = new Date().getTime() - this.startTime;
+
+                expect(todoEndTime).not.toBeLessThan(0);
+                expect(todoEndTime).not.toBeGreaterThan(endTime);
+
+                // Unset done
+                this.todo.set('done', false);
+
+                expect(this.todo.data.completedDate).toBe(null);
             });
         });
-        describe('by object', function() {
-            it('should update task name', function () {
-                var taskName = 'Do this later',
-                    newTodoData = {
-                        task: taskName
-                    };
 
-                this.todo.set(newTodoData);
+        describe('set fields by object', function() {
+            it('should not set done or completedDate when changing another field', function () {
+                // Verify the constructor does not complete the item
+                expect(this.todo.data.done).toBe(false);
+                expect(this.todo.data.completedDate).toBe(null);
+
+                // Set a field
+                this.todo.set({task: taskName});
+
                 expect(this.todo.data.task).toBe(taskName);
                 expect(this.todo.data.done).toBe(false);
                 expect(this.todo.data.completedDate).toBe(null);
             });
 
             it('should complete the todo', function () {
-                var newTodoData = {
-                    done: true
-                };
-                this.todo.set(newTodoData);
+                // Verify the constructor does not complete the item
+                expect(this.todo.data.done).toBe(false);
+                expect(this.todo.data.completedDate).toBe(null);
+
+                // Complete the item
+                this.todo.set({done: true});
 
                 expect(this.todo.data.done).toBe(true);
                 expect(this.todo.data.completedDate).not.toBe(null);
 
+                // Calculate times
                 var todoEndTime = this.todo.data.completedDate.getTime() - this.startTime,
-                    testEndTime = new Date().getTime() - this.startTime;
+                    endTime = new Date().getTime() - this.startTime;
 
-                expect(todoEndTime).toBeGreaterThan(0);
+                expect(todoEndTime).not.toBeLessThan(0);
+                expect(todoEndTime).not.toBeGreaterThan(endTime);
+            });
 
-                if (todoEndTime < testEndTime) {
-                    expect(todoEndTime).toBeLessThan(testEndTime);
-                } else {
-                    expect(todoEndTime).toBe(testEndTime);
-                }
+            it('should unset completedDate when done is unset', function () {
+                // Verify the constructor does not complete the item
+                expect(this.todo.data.done).toBe(false);
+                expect(this.todo.data.completedDate).toBe(null);
+
+                // Complete the item
+                this.todo.set({done: true});
+
+                expect(this.todo.data.done).toBe(true);
+                expect(this.todo.data.completedDate).not.toBe(null);
+
+                // Calculate times
+                var todoEndTime = this.todo.data.completedDate.getTime() - this.startTime,
+                    endTime = new Date().getTime() - this.startTime;
+
+                expect(todoEndTime).not.toBeLessThan(0);
+                expect(todoEndTime).not.toBeGreaterThan(endTime);
+
+                // Unset done
+                this.todo.set({done: false});
+
+                expect(this.todo.data.completedDate).toBe(null);
             });
         });
     });
@@ -83,41 +140,108 @@ describe('Todo Tests', function() {
             this.todo = new Admin.model.Todo(this.data);
         });
 
-        describe('by field name', function() {
-            it('should update task name', function () {
-                var taskName = 'Do this later';
+        describe('set fields by name', function() {
+            it('should not change done or completedDate when changing another field', function () {
+                // Verify the item's state
+                expect(this.todo.data.done).toBe(true);
+                expect(this.todo.data.completedDate).not.toBe(null);
+                var completedDate = this.todo.data.completedDate.getTime();
+
+                // Set a field
                 this.todo.set('task', taskName);
+
                 expect(this.todo.data.task).toBe(taskName);
-                expect(this.todo.data.completedDate).toBe(this.data.completedDate);
+                expect(this.todo.data.done).toBe(true);
+                expect(this.todo.data.completedDate).not.toBe(null);
+                expect(this.todo.data.completedDate.getTime()).toBe(completedDate);
             });
 
-            it('should un-complete the todo', function () {
+            it('should unset completedDate when done is unset', function () {
+                // Verify the constructor does not complete the item
+                expect(this.todo.data.done).toBe(true);
+                expect(this.todo.data.completedDate).not.toBe(null);
+
+                // Unset done
                 this.todo.set('done', false);
 
-                expect(this.todo.data.done).toBe(false);
                 expect(this.todo.data.completedDate).toBe(null);
+            });
+
+            it('should reset completedDate when done is reset', function () {
+                // Verify the item's state
+                expect(this.todo.data.done).toBe(true);
+                expect(this.todo.data.completedDate).not.toBe(null);
+                var completedDate = this.todo.data.completedDate.getTime();
+
+                // Unset done
+                this.todo.set('done', false);
+
+                expect(this.todo.data.completedDate).toBe(null);
+
+                // Complete the item
+                this.todo.set('done', true);
+
+                expect(this.todo.data.completedDate).not.toBe(null);
+                expect(this.todo.data.completedDate.getTime()).not.toBe(completedDate);
+
+                // Calculate times
+                var todoEndTime = this.todo.data.completedDate.getTime() - this.startTime,
+                    endTime = new Date().getTime() - this.startTime;
+
+                expect(todoEndTime).not.toBeLessThan(0);
+                expect(todoEndTime).not.toBeGreaterThan(endTime);
             });
         });
-        describe('by object', function() {
-            it('should update task name', function () {
-                var taskName = 'Do this later',
-                    newTodoData = {
-                        task: taskName
-                    };
 
-                this.todo.set(newTodoData);
+        describe('set fields by object', function() {
+            it('should not change done or completedDate when changing another field', function () {
+                // Verify the item's state
+                expect(this.todo.data.done).toBe(true);
+                expect(this.todo.data.completedDate).not.toBe(null);
+                var completedDate = this.todo.data.completedDate.getTime();
+
+                // Set a field
+                this.todo.set({task: taskName});
+
                 expect(this.todo.data.task).toBe(taskName);
-                expect(this.todo.data.completedDate).toBe(this.data.completedDate);
+                expect(this.todo.data.done).toBe(true);
+                expect(this.todo.data.completedDate.getTime()).toBe(completedDate);
             });
 
-            it('should un-complete the todo', function () {
-                var newTodoData = {
-                    done: false
-                };
-                this.todo.set(newTodoData);
+            it('should unset completedDate when done is unset', function () {
+                // Verify the constructor does not complete the item
+                expect(this.todo.data.done).toBe(true);
+                expect(this.todo.data.completedDate).not.toBe(null);
+
+                // Unset done
+                this.todo.set({done: false});
+
+                expect(this.todo.data.completedDate).toBe(null);
+            });
+
+            it('should reset completedDate when done is reset', function () {
+                // Verify the item's state
+                expect(this.todo.data.done).toBe(true);
+                expect(this.todo.data.completedDate).not.toBe(null);
+                var completedDate = this.todo.data.completedDate.getTime();
+
+                // Unset done
+                this.todo.set({done: false});
 
                 expect(this.todo.data.done).toBe(false);
                 expect(this.todo.data.completedDate).toBe(null);
+
+                // Complete the item
+                this.todo.set({done: true});
+                expect(this.todo.data.completedDate).not.toBe(null);
+                expect(this.todo.data.completedDate.getTime()).not.toBe(completedDate);
+
+                // Calculate times
+                var todoEndTime = this.todo.data.completedDate.getTime() - this.startTime,
+                    endTime = new Date().getTime() - this.startTime;
+
+                expect(todoEndTime).not.toBeLessThan(0);
+                expect(todoEndTime).not.toBeGreaterThan(endTime);
             });
         });
     });
